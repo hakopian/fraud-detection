2b9c908a9f45b4c2c7e0afb84b632bb8d6ee4af1
hakopian <130099663+hakopian@users.noreply.github.com>
2024-07-19 12:20:37 -0700

    port adjustments

--- fraud_detection_model.py
+++ fraud_detection_model.py
@@ -12,21 +12,21 @@ def train_and_predict_model(train_file_path, test_file_path):
     test_data = pd.read_csv(test_file_path)
     X_test = test_data[['amount', 'merchant', 'location']]
     
-    # One-Hot Encoding for categorical features in both datasets
+    # one hot Encoding for categorical features in datasets
     X_train = pd.get_dummies(X_train)
     X_test = pd.get_dummies(X_test)
 
-    # Align columns in the test set to match those in the training set
+    # aligning columns in test to match train
     X_test = X_test.reindex(columns = X_train.columns, fill_value=0)
     
-    # Train the model
+    # train the model
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
-    # Make predictions
+    # make predictions
     predictions = model.predict(X_test)
     
-    # Save the predictions
+    # save the predictions
     test_data['fraudulent'] = predictions
     test_data.to_csv('predicted_test_transaction_data.csv', index=False)
 
